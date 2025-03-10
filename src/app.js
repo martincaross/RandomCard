056be4332e4d7a4d076e9e80c16b274895247830
Martín <martinjcaro@gmail.com>
2025-03-10 20:43:00 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  document.getElementById("newCard").addEventListener("click", generateCard);
+  setInterval(generateCard, 10000);
 };
+
+function getRandomFromArray(values) {
+  return values[Math.floor(Math.random() * values.length)];
+}
+
+function generateCard() {
+  let palo = getRandomFromArray(["diamante", "corazon", "pica", "trebol"]);
+  let valor = getRandomFromArray(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]);
+  let imagen = `./assets/img/${palo}.png`; // Asegúrate de que la ruta sea correcta
+
+  document.getElementById("palo-top").src = imagen;
+  document.getElementById("palo-bottom").src = imagen;
+  document.getElementById("valor").innerText = valor;
+
+  document.getElementById("valor").style.color = (palo === "corazon" || palo === "diamante") ? "red" : "black";
+}
